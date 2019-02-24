1b112b5994244181fa951710953d61012b683d76
Peshal Bhandari <adampeshal17@gmail.com>
2019-02-24 12:11:36 -0500

    added logger and debug controller

---
+++ ORBSAPI/ORBSAPI/Controllers/DebugController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace ORBSAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Debug")]
+    public class DebugController : Controller
+    {
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(new
+            {
+                projectName = Assembly.GetExecutingAssembly().FullName,
+                CurrentDate = System.DateTime.Now,
+                PoweredBy = "Perfect Active Solutions"
+            });
+        }
+    }
+}
--- ORBSAPI/ORBSAPI/Startup.cs
+++ ORBSAPI/ORBSAPI/Startup.cs
@@ -1,33 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace ORBSAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+                });
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+
+            //add CORS to the application
+            var cors = Configuration["CORS:Origin"];
+            if (cors != null)
+            {
+                services.AddCors(x => x.AddPolicy("corsPolicy", y => y.WithOrigins(cors).AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +54,13 @@ namespace ORBSAPI
             }
 
             app.UseMvc();
+            env.ConfigureNLog("nlog.config");
+            // make sure Chinese chars don't fk up
+            // Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            //add NLog to ASP.NET Core
+            loggerFactory.AddNLog();
+
         }
     }
 }
